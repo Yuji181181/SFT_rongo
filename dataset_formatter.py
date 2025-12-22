c81caefd83045bc1cffd2181720fad901de0634b
Yuji181181 <s1f102402552@iniad.org>
2025-12-22 17:40:35 +0900

    formatter

--- dataset_formatter.py
+++ dataset_formatter.py
@@ -1,13 +1,12 @@
-"""データセットのフォーマットを整える"""
 from datasets import load_dataset
-if 'get_ipython' not in globals(): # Colab環境でない場合は
+if 'get_ipython' not in globals():
     from config import *
 
-# データセットを読み込んで最大件数だけ取り出す --- (*1)
+# データセットを読み込んで最大件数だけ取り出す
 dataset = load_dataset(DATASET_NAME, split="train")
 dataset = dataset.select(range(MAX_DATASET_SIZE))
 
-# プロンプトテンプレートの指定 --- (*2)
+# プロンプトテンプレートの指定
 PROMPT_TEMPLATE = """\
 <s>あなたは偉い先生である。次のタスクに対する指示文に対して内容に基づいて論語風に答えよ。
 
@@ -18,14 +17,14 @@ PROMPT_TEMPLATE = """\
 子曰く、{}</s>
 """
 
-# データセットを整形する関数を用意 --- (*3)
+# データセットを整形する関数を用意
 def format_example(example):
     """テキストを整形する関数"""
-    # データセットから値を取り出す --- (*4)
+    # データセットから値を取り出す
     instruction = example["instruction"].strip()
     input = example["input"].strip()
     output = example["output"].strip()
-    # 応答を「である調」に変換する --- (*5)
+    # 応答を「である調」に変換する
     output = output.replace("です。", "である。")
     output = output.replace("ちます。", "つ。")
     output = output.replace("でした", "だった")
@@ -33,10 +32,10 @@ def format_example(example):
     output = output.replace("します", "するのだ")
     output = output.replace("あります。", "あるのだ。")
     output = output.replace("してください", "してみよ")
-    # プロンプトテンプレートに埋め込む --- (*6)
+    # プロンプトテンプレートに埋め込む
     text = PROMPT_TEMPLATE.format(instruction, input, output)
     print("text=", text)
     return {"text": text}
 
-# データセットの整形を適用 --- (*7)
-dataset = dataset.map(format_example) # 整形を適用
+# データセットの整形を適用
+dataset = dataset.map(format_example)
