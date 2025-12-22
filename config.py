4358160d2e72d9e0932386e30dcaa8559fdf1cd2
Yuji181181 <s1f102402552@iniad.org>
2025-12-22 17:14:32 +0900

    config

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-"""Fine-Tuningの設定"""
-# モデルとデータセットの指定 --- (*1)
 MODEL_NAME = "unsloth/mistral-7b-bnb-4bit"
 DATASET_NAME = "shi3z/alpaca_cleaned_ja_json"
-# データセットのサイズやモデルを制限するための変数 --- (*2)
-MAX_DATASET_SIZE = 500
-MAX_SEQ_LENGTH = 2048
-MAX_STEPS = 100 # 学習のステップ数 (テストのときは1) --- (*3)
+
+MAX_DATASET_SIZE = 500 ## データセットの最大サイズ
+MAX_SEQ_LENGTH = 2048 ## シーケンス長
+MAX_STEPS = 1 ## 学習ステップ数
+
 # モデルの保存先ディレクトリ
-MODEL_SAVE_DIR="./rongofu_llm"
+MODEL_SAVE_DIR="./rongocho_llm"
