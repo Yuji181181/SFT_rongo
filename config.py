9e8b11733b0acf2b591ed5813ee8117c388a9476
Yuji181181 <s1f102402552@iniad.org>
2025-12-23 19:38:36 +0900

    finish

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DATASET_NAME = "shi3z/alpaca_cleaned_ja_json"
 
 MAX_DATASET_SIZE = 500  ## データセットの最大サイズ
 MAX_SEQ_LENGTH = 2048  ## シーケンス長
-MAX_STEPS = 1  ## 学習ステップ数
+MAX_STEPS = 100  ## 学習ステップ数
 
 # モデルの保存先ディレクトリ
 MODEL_SAVE_DIR = "./rongocho_llm"
